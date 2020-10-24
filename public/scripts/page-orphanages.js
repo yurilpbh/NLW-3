454a8c1f241c41e6e72445a86dabb0928e5ab34b
yurilpbh <yurilpbh@hotmail.com>
2020-10-24 12:04:40 -0300

    Implementacoes alem da proposta da semana

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,8 +5,82 @@
 // Boolean true or false
 // Array []
 
+var jsonEstados = [];
+$.getJSON('./json/estados.json', function(data) {
+  $.each(data, function(key,val){
+    jsonEstados[key] = val; 
+  }); 
+});
+
+var jsonCidades = [];
+$.getJSON('./json/municipios.json', function(data) {
+  $.each(data, function(key,val){
+    jsonCidades[key] = val; 
+  }); 
+});
+
+var geoJsonMunicipio = [];
+$.getJSON('./json/municipio.json', function(data) {
+  $.each(data, function(key,val){
+    geoJsonMunicipio[key] = val; 
+  }); 
+});
+
 const orphanagesSpan = document.querySelectorAll('.orphanages span')
-var lat = 0, lng = 0, count = 0, fitBounds = [[Infinity,Infinity],[-Infinity,-Infinity]];
+var lat = 0, lng = 0, count = 0, fitBounds = [[Infinity,Infinity],[-Infinity,-Infinity]], ultimoEstado;
+var latitude=-14.9, longitude=-49.5, layerGeoJson;;
+
+function getLatLngEstados(){
+  if(document.getElementById("estado").value != ""){
+    map.setView([latitude, longitude],6);
+    if(ultimoEstado != document.getElementById("estado").value){
+      ultimoEstado = document.getElementById("estado").value;
+      var estado = jsonEstados.find(estado => estado.uf == document.getElementById("estado").value)
+      if(latitude != estado.latitude || longitude != estado.longitude){
+        latitude = estado.latitude;
+        longitude = estado.longitude;
+        map.setView([latitude, longitude],6);
+
+        var first = true, geoJsonCidades;
+        geoJsonMunicipio.features.forEach(function (a) {
+          if(a.properties.UF == document.getElementById("estado").value){
+            if(first){
+              geoJsonCidades = [a];
+              first = false;
+            }else{
+              geoJsonCidades = geoJsonCidades.concat(a);
+              return;
+            }
+          }
+        })
+        if(map.hasLayer(layerGeoJson)){
+          map.removeLayer(layerGeoJson);
+        }
+        layerGeoJson = L.geoJSON(geoJsonCidades);
+        map.addLayer(layerGeoJson)
+      }
+    }
+  }
+}
+
+function getLatLngCidades(){
+  if(document.getElementById("cidade").value != ""){
+    var cidadeSelecionada;
+    jsonEstados.find(function (estado) {
+      if(estado.uf == document.getElementById("estado").value){
+        jsonCidades.find(function (cidade) {
+          if(estado.codigo_uf == cidade.codigo_uf && cidade.nome == document.getElementById("cidade").value){
+            cidadeSelecionada = cidade;
+            return;
+          }
+        })
+      }
+    })
+    latitude = cidadeSelecionada.latitude;
+    longitude = cidadeSelecionada.longitude;
+    map.setView([latitude, longitude],12);
+  }
+}
 
 orphanagesSpan.forEach(span => {
   var aux = parseFloat(span.dataset.lat);
@@ -17,10 +91,14 @@ orphanagesSpan.forEach(span => {
   fitBounds[1][1] = Math.max(aux, fitBounds[1][1]);
 })
 
-//var zoom = lat;
+var myStyle = {
+  "color": "blue",
+  "weight": 1,
+  "opacity": 0.01
+};
 
 //Create map
-const map = L.map('mapid').fitBounds(fitBounds);
+const map = L.map('mapid').setView([latitude, longitude],4);
 
 // Create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -79,3 +157,4 @@ map.on('click',(event) => {
   marker = L.marker([lat, lng], {icon})
   .addTo(map)
 })
+
